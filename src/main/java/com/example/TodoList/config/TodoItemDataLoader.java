06ab589ca1a0d3a1c386ac03630b2f3817834c61
Yuchen Cao <caoyc@bu.edu>
2024-07-18 13:37:55 -0400

    Add relationships between front-end and back-end to make front-end show data from back-end

---
+++ src/main/java/com/example/TodoList/config/TodoItemDataLoader.java
@@ -0,0 +1,36 @@
+package com.example.TodoList.config;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.example.TodoList.models.TodoItem;
+import com.example.TodoList.repositories.TodoItemRepository;
+
+@Component
+public class TodoItemDataLoader implements CommandLineRunner{
+    private final Logger logger = LoggerFactory.getLogger(TodoItemDataLoader.class);
+
+    @Autowired
+    TodoItemRepository todoItemRepository;
+
+    @Override
+    public void run(String... args) throws Exception{
+        loadSeedData();
+    }
+
+    private void loadSeedData(){
+        if(todoItemRepository.count() == 0){
+            TodoItem todoItem1 = new TodoItem("get the milk");
+            TodoItem todoItem2 = new TodoItem("rake the leaves");
+        
+            todoItemRepository.save(todoItem1);
+            todoItemRepository.save(todoItem2);
+        }
+        logger.info("Number of TodoItems: {}", todoItemRepository.count());
+    }
+}
+
--- src/main/java/com/example/TodoList/controllers/TodoItemController.java
+++ src/main/java/com/example/TodoList/controllers/TodoItemController.java
@@ -1,21 +1,27 @@
 package com.example.TodoList.controllers;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.example.TodoList.repositories.TodoItemRepository;
 
 
 @Controller
 public class TodoItemController {
     private final Logger logger = LoggerFactory.getLogger(TodoItemController.class);
 
+    @Autowired
+    private TodoItemRepository todoItemRepository;
+
     @GetMapping("/")
     public ModelAndView index(){
-        logger.debug("request to GET index");
+        logger.info("request to GET index");
         ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("todoItems", todoItemRepository.findAll());
         return modelAndView;
     }
     
