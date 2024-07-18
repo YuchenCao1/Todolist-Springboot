9755cebaf6a0d0b8820416ed6f56b5466cc2c784
Yuchen Cao <caoyc@bu.edu>
2024-07-18 14:36:27 -0400

    Add Edit and Delete Function

---
+++ src/main/java/com/example/TodoList/controllers/TodoFormController.java
@@ -0,0 +1,36 @@
+package com.example.TodoList.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.TodoList.models.TodoItem;
+import com.example.TodoList.repositories.TodoItemRepository;
+
+@Controller
+public class TodoFormController {
+     @Autowired
+    private TodoItemRepository todoItemRepository;
+
+    @GetMapping("/edit/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        TodoItem todoItem = todoItemRepository
+        .findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("TodoItem id: " + id + " not found"));
+    
+        model.addAttribute("todo", todoItem);
+        return "update-todo-item";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteTodoItem(@PathVariable("id") long id, Model model) {
+        TodoItem todoItem = todoItemRepository
+        .findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("TodoItem id: " + id + " not found"));
+    
+        todoItemRepository.delete(todoItem);
+        return "redirect:/";
+    }
+}
